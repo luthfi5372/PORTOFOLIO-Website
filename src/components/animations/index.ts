e8786a25edec1a77d1bfc69ce69b6f4df2e79b1a
luthfi5372 <luthfi5372@gmail.com>
2026-03-10 01:32:30 +0700

    Final: Innovation Synthesis System Launched

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -1,6 +1,5 @@
 export { default as SmoothScroll } from "./SmoothScroll";
 export { default as FadeIn } from "./FadeIn";
 export { default as NeuralFlowBackground } from "./NeuralFlowBackground";
-export { default as LiquidBackground } from "./LiquidBackground";
 export { default as GravityBadge } from "./GravityElement";
 
